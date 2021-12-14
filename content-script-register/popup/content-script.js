4c4da03e2b9d84b0f8e29619d7ecf8cfde165e96
Kurt Apple <kapplepi@gmail.com>
2021-12-14 08:57:14 -0800

    proof of concept and readme

--- content-script-register/popup/content-script.js
+++ content-script-register/popup/content-script.js
@@ -3,8 +3,49 @@
 const hostsInput =  document.querySelector("#hosts");
 const codeInput =  document.querySelector("#code");
 
-const defaultHosts = "*://*.org/*";
-const defaultCode = "document.body.innerHTML = '<h1>This page has been eaten</h1>'";
+const defaultHosts = "*://*.com/*";
+//this one works on div tags
+/*const defaultCode = "\
+foundElements = document.getElementsByTagName('div');\
+for(i = 0; i < foundElements.length; i++) {\
+  foundElements[i].innerHTML = '<h1>fart</h1>';\
+}";*/
+//attempting to find all with a certain class ID now
+const defaultCode = "\
+console.log('hi');\
+wordlist=['keyword',\
+          'another'];\
+censorThis = function() {\
+  foundElements = document.querySelectorAll('div.ytd-rich-grid-media .ytd-watch-next-secondary-results-renderer');\
+  for(i = 0; i < foundElements.length; i++) {\
+    allTextContent = foundElements[i].innerText.toLowerCase();\
+    if(wordlist.some(function(v) { return allTextContent.indexOf(v.toLowerCase()) >= 0; })) {\
+      console.log('ganked');\
+      distractingMeta = foundElements[i].getElementsByTagName('a');\
+      for(j = 0; j < distractingMeta.length; j++) {\
+        distractingMeta[j].innerHTML = 'blocked';\
+        distractingMeta[j].href = 'https://google.com';\
+      }\
+    }\
+  }\
+};\
+censorThis();\
+console.log('hi');\
+let lastUrl;\
+const observer = new MutationObserver(mutations => {\
+  if(location.href == lastUrl)\
+    return;\
+  lastUrl = location.href;\
+  censorThis();\
+});\
+const config = {\
+  childList: true,\
+  subtree: true\
+};\
+observer.observe(document.body, config);\
+";
+
+const testLog = "alert('alert'); console.log('log');";
 
 hostsInput.value = defaultHosts;
 codeInput.value = defaultCode;
